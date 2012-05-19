5a67f4f05790abdb20db2ee6b79d737d307e9a2b
Alexandros Salazar <nomothetis@gmail.com>
2012-05-18 20:52:08 -0400

    Made git settings public.

--- config.fish
+++ config.fish
@@ -1,3 +1,25 @@
+set -x LSCOLORS 'gxfxfxfxcxdxdxhbadbxbx'
+
+function parse_git_branch
+  # make sure the directory is a git repo 
+  if not is-git
+    return
+  end
+            
+  if has-git-changes
+    set_color white
+  else
+    set_color white
+  end
+  echo -n ' /'
+  set -l branch (env git symbolic-ref -q HEAD)
+  if test ! -z "$branch"
+    echo -n $branch | sed -e 's/refs\/heads\///'
+  else
+    git name-rev --name-only HEAD ^/dev/null
+  end
+end
+
 for p in /usr/bin /usr/local/bin /usr/local/mysql/bin ~/.gem/bin ~/.gem/ruby/1.8/bin ~/bin ~/.config/fish/bin 
 	if test -d $p
 		set -x PATH $p $PATH
@@ -19,4 +41,10 @@ set -x JAVA_HOME "/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home"
 # pull in a local, ignored-by-git config file
 if test -f ~/.config/fish/config.local.fish
   . ~/.config/fish/config.local.fish
+end
+
+function fish_prompt --description "Write out the prompt"
+  printf "%s%s%s%s%s:" (set_color cyan) (prompt_pwd) (parse_git_branch) (set_color cyan)
+  set_color normal
+  printf ' '
 end
